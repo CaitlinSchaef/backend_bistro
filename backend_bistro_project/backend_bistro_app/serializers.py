3daf48cf1bcf2e071447636181ed2b198a3b8644
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-22 17:06:36 -0400

    set up views and serializer

---
+++ backend_bistro_project/backend_bistro_app/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import *
+
+# class StudentSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Student
+#        #what model did we attach it to? Student
+#        #what fields do we want to translate:
+#        fields = ['id', 'name', 'age', 'courses']
+
--- backend_bistro_project/backend_bistro_app/views.py
+++ backend_bistro_project/backend_bistro_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+
+from .models import *
+#can do .models as opposed to a more explicit path because we're in same folder
+
+
+from .serializers import *
+# we are going to use the serialized data here so we need it all
 
 # Create your views here.
+
+# class StudentViewSet(viewsets.ModelViewSet):
+#    #two things going on that are pretty much always there, queryset based on what we want to look at (which you can filter down but we won't here)
+#    #this is all the students
+#    queryset = Student.objects.all()
+#    #and what serializer to use, it will be the student serializer
+#    #this is basically the endpoint, but we need to register it in the url
+#    serializer_class = StudentSerializer
